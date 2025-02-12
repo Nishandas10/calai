ff969fd640ad18de51c9e472178535fe2ffdff26
Nishant Das <dasbudhe@gmail.com>
2025-02-12 06:43:00 +0530

    supabase

--- supabase/functions/analyze-food/index.ts
+++ supabase/functions/analyze-food/index.ts
@@ -3,7 +3,7 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 // @ts-ignore: Deno imports
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.1";
 // @ts-ignore: Deno imports
-import { OpenAI } from "https://esm.sh/openai@4.28.0";
+import { OpenAI } from "https://cdn.skypack.dev/openai@4.28.0?dts";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
